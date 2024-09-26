566382a812af89007ae525f7cb38ebc7bcc85f61
tmaillar <tmaillar@student.42.fr>
2024-09-26 15:01:58 +0200

    done

--- first/script.js
+++ first/script.js
@@ -1,18 +1,4 @@
-// document.querySelector('.open-modal').addEventListener('click', function(event) {
-//     event.preventDefault();
-//     document.getElementById('modal').style.display = 'block';
-// });
 
-// document.querySelector('.close').addEventListener('click', function() {
-//     document.getElementById('modal').style.display = 'none';
-// });
-
-// window.addEventListener('click', function(event) {
-//     const modal = document.getElementById('modal');
-//     if (event.target === modal) {
-//         modal.style.display = 'none';
-//     }
-// });
 // Obtenir toutes les modales
 const modals = document.querySelectorAll('.modal');
 // Obtenir les liens pour ouvrir les modales
@@ -48,3 +34,4 @@ window.addEventListener('click', function(event) {
         }
     });
 });
+
